85a357b1cf84b600b625bfd9bee43ca72721d03e
daong <daong@DESKTOP-AHVIFTP>
2020-07-14 01:47:51 +0700

    Add project files.

---
+++ PlayvideoUSB_CDC/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace PlayvideoUSB_CDC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cbxComlist = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(62, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Top Left";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bot Right";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(62, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 96);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 41;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cbxComlist
+            // 
+            this.cbxComlist.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxComlist.FormattingEnabled = true;
+            this.cbxComlist.Location = new System.Drawing.Point(76, 182);
+            this.cbxComlist.Name = "cbxComlist";
+            this.cbxComlist.Size = new System.Drawing.Size(62, 21);
+            this.cbxComlist.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(144, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // serialPort
+            // 
+            this.serialPort.BaudRate = 3686400;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(16, 225);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(105, 137);
+            this.groupBox1.TabIndex = 32;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gamma";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(19, 107);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(82, 17);
+            this.checkBox3.TabIndex = 30;
+            this.checkBox3.Text = "Use gamma";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(65, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(32, 20);
+            this.textBox3.TabIndex = 22;
+            this.textBox3.Text = "2.8";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "MaxOut";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Gamma";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(66, 55);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(31, 20);
+            this.textBox4.TabIndex = 28;
+            this.textBox4.Text = "255";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(66, 81);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(31, 20);
+            this.textBox5.TabIndex = 26;
+            this.textBox5.Text = "255";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "MaxIn";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "bit pwm";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox8);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(128, 225);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(79, 137);
+            this.groupBox2.TabIndex = 34;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Setup";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(8, 81);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(18, 20);
+            this.textBox8.TabIndex = 36;
+            this.textBox8.Text = "32";
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 84);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "height";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(8, 55);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(18, 20);
+            this.textBox7.TabIndex = 34;
+            this.textBox7.Text = "64";
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "width";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(8, 29);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(18, 20);
+            this.textBox6.TabIndex = 31;
+            this.textBox6.Text = "8";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 371);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.cbxComlist);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Play";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox cbxComlist;
+        private System.Windows.Forms.Button button3;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ PlayvideoUSB_CDC/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO.Ports;
+namespace PlayvideoUSB_CDC
+{
+    public partial class Form1 : Form
+    {
+        int x0, y0, x1, y1,type;
+        int width=64, height=32;
+        double gamma=2.8;
+        int max_in=255;
+        int max_out=255;
+        int bitpwm = 8;
+        Boolean play = false;
+        public Form1()
+        {
+            InitializeComponent();
+            MouseHook.MouseAction_move += new EventHandler(mouse_move);
+            MouseHook.MouseAction_click += new EventHandler(mouse_click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ComList = SerialPort.GetPortNames();
+            int[] ComNumberList = new int[ComList.Length];
+            for (int i = 0; i < ComList.Length; i++)
+            {
+                ComNumberList[i] = int.Parse(ComList[i].Substring(3));
+            }
+            Array.Sort(ComNumberList);
+            foreach (int ComNumber in ComNumberList)
+            {
+                cbxComlist.Items.Add("COM" + ComNumber.ToString());
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            play = !play;
+            if (play == true)
+            {
+                timer1.Enabled = true;
+                button2.Text = "Stop";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                button2.Text = "Play";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap bmp_data = new Bitmap(width,height);
+            try
+            {
+                Bitmap bmp = new Bitmap(x1 - x0 + 1, y1 - y0 + 1);
+                Graphics gr = Graphics.FromImage(bmp);
+                gr.CopyFromScreen(x0, y0, 0, 0, bmp.Size);
+                bmp_data  = ResizeBitmap(bmp, 192, 96);
+                pictureBox1.Image = bmp_data;
+            }
+            catch
+            {
+
+            }
+            if (serialPort.IsOpen) send_data(bmp_data);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (cbxComlist.Text == "")
+            {
+                MessageBox.Show("Vui lòng chọn cổng com", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                button3.Text = "Kết nối";
+                cbxComlist.Enabled = true;
+            }
+            else
+            {
+                serialPort.PortName = cbxComlist.Text;
+                try
+                {
+                    serialPort.Open();
+                    button3.Text = "Ngắt kết nối";
+                    cbxComlist.Enabled = false;
+                }
+                catch
+                {
+                    MessageBox.Show("Không thể mở cổng " + serialPort.PortName, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            width = Int16.Parse(textBox7.Text);
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            height = Int16.Parse(textBox8.Text);
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            bitpwm = Int16.Parse(textBox6.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            gamma = Convert.ToDouble(textBox3.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            max_out = Int16.Parse(textBox4.Text);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            max_in = Int16.Parse(textBox5.Text);
+        }
+
+        private void mouse_move(object sender, EventArgs e)
+        {
+            if (type == 1)
+            {
+                x0 = System.Windows.Forms.Control.MousePosition.X;
+                y0 = System.Windows.Forms.Control.MousePosition.Y;
+                textBox1.Text = x0.ToString() + "," + y0.ToString();
+            }
+            else if (type == 2)
+            {
+                x1 = System.Windows.Forms.Control.MousePosition.X;
+                y1 = System.Windows.Forms.Control.MousePosition.Y;
+                textBox2.Text = x1.ToString() + "," + y1.ToString();
+            }
+        }
+        private void mouse_click(object sender, EventArgs e)
+        {
+            if (type == 1)
+                type = 2;             
+            else
+            {
+                type = 0;
+                button1.Enabled = true;
+                Bitmap bmp = new Bitmap(x1 - x0 + 1, y1 - y0 + 1);
+                Graphics gr = Graphics.FromImage(bmp);
+                gr.CopyFromScreen(x0, y0, 0, 0, bmp.Size);
+                pictureBox1.Image = ResizeBitmap(bmp, 192, 96);
+                MouseHook.stop();
+            }    
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            type = 1;
+            timer1.Enabled = false;
+            button2.Text = "Play";
+            x0 = y0 = x1 = y1 = 0;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            button1.Enabled = false;
+            MouseHook.Start();
+        }
+
+        //resize imgae
+        public Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+
+            return result;
+        }
+        void send_data(Bitmap bitmap)
+        {
+            Bitmap bmp = ResizeBitmap(bitmap,width,height);
+            byte[] bytestosend = new byte[64];
+            for (int i = 0; i < 3; i++) //3 màu RGB 
+            {
+                for (int j = 0; j < height; j++)        // quét từng hàng ngang 1
+                {
+                    for (int index = 0; index < width / 64; index++)  //quét từng tấm 1
+                    {
+                        for (int k = 0; k < 64; k++)                        //1 lần gửi 64 byte
+                        {
+                            Color curColor = bmp.GetPixel(k + (index * 64), j);
+
+                            if (i == 0) bytestosend[k] = curColor.R;
+                            else if (i == 1) bytestosend[k] = curColor.G;
+                            else bytestosend[k] = curColor.B;              //lấy ra màu của pixel
+
+                            if (checkBox3.Checked == true) bytestosend[k] = (byte)(Math.Pow((double)bytestosend[k] / (double)max_in, gamma) * max_out + 0.5);        //tính toán gamma
+                        }
+                        serialPort.Write(bytestosend, 0, 64); //gửi 1 gói tin đi
+                    }
+                }
+            }
+        }
+    }
+
+    public static class MouseHook
+    {
+        public static event EventHandler MouseAction_click = delegate { };
+        public static event EventHandler MouseAction_move = delegate { };
+
+        public static void Start()
+        {
+            _hookID = SetHook(_proc);
+
+
+        }
+        public static void stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                  GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+          int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseAction_click(null, new EventArgs());
+            }
+            if (nCode >= 0 && MouseMessages.WM_MOUSEMOVE == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseAction_move(null, new EventArgs());
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_MOUSE_LL = 14;
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+          LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+          IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+
+    }
+}
